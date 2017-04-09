2c50a76722172f96377dca6bc24fe42256e0ff3f
paulo9mv <paulo9mv@yahoo.com.br>
2017-04-09 03:30:55 -0400

    Main
    Main

--- ProjArvoreBinaria/main.c
+++ ProjArvoreBinaria/main.c
@@ -25,21 +25,13 @@ char *devolvePalavra(char palavra[]){
 }
 int main()
 {
-    int tmp;
-<<<<<<< HEAD
-    char palavra[21], aux[21];
-=======
-
-     ArvBin palavra;
-    //const char fim[3]= "\0";
+    int tmp, i;
     char aux[21];
->>>>>>> origin/master
 
     ArvBin* raiz = cria_ArvBin();
     FILE *entrada = fopen("entrada.txt","r");
     setlocale(LC_ALL, "Portuguese");
 
-    int i=0,j;
     for(i = 0, tmp = getc(entrada); tmp != EOF; i++, tmp = getc(entrada)){
         if((tmp != ' ') && (tmp != ',') && (tmp != ';') && (tmp != '.') && (tmp !='\n') && (tmp !='\0') && (tmp !='?') && (tmp !='!')){
             if(tmp>=65 &&tmp<91)
@@ -53,40 +45,7 @@ int main()
             i = -1;
         }
     }
-<<<<<<< HEAD
-=======
-    /*while(!feof(entrada) && !ferror(entrada))
-    {
-        // ate o fim do arquivo
 
-        tmp = fgetc(entrada); // lendo dados
-
-        while((tmp != ' ') && (tmp != ',') && (tmp != ';') && (tmp != '.') && (tmp !='\n') && (tmp !='\0') && (tmp !='?') && (tmp !='!')) //condicoes para pegar as palavras
-        {
-            if(tmp>=65 &&tmp<91){
-                tmp=tmp+32;
-                }
-            aux[i]=tmp;
-            //printf("aux:%c eh tmp=%d\n",aux[i],tmp);
-            tmp =fgetc(entrada);
-            if(tmp == EOF) //fim do arquivo
-                break;
-            i++;
-        }
-
-        aux[i]='\0';
-        printf("%s\n",aux);
-        //printf("aux:%s\n",aux);
-//        *palavra = strtok(aux,fim);
-        strcpy(palavra->info,aux);
-
-        insere_ArvBin(raiz,palavra->info);
-
-       // printf("palavra:%s     aux:%s\n",palavra,aux);
-
-
-    }*/
->>>>>>> origin/master
 
 
     //preOrdem_ArvBin(raiz);
